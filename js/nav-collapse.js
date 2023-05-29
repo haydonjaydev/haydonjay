172a57662707e69241cf222ce3b0eac27556a856
haydonjaydev <haydonjaydev@gmail.com>
2023-05-29 14:38:38 +0100

    Updates to the repo

--- js/nav-collapse.js
+++ js/nav-collapse.js
@@ -4,6 +4,8 @@ var i;
     for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
         this.classList.toggle("active");
+        this.getElementsByClassName("btn")[0].classList.toggle("fa-chevron-right");
+        this.getElementsByClassName("btn")[0].classList.toggle("fa-chevron-down");
         var content = this.nextElementSibling;
         if (content.style.maxHeight){
         content.style.maxHeight = null;
